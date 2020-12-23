2a129e19f812af636a753364de44ffd66a2ffac7
18616753564 <m18616753564@163.com>
2020-12-23 15:05:57 +0800

    修改runner

--- test/test_sms.py
+++ test/test_sms.py
@@ -37,17 +37,16 @@ class TestSms:
         assert res.json()['status'] == 0
 
 
-
-    def test_MaterialList(self,getheaders):
-        gmtStartDate = [None, "2020-08-03 00:00:00"]
-        gmtEndDate = [None, "2021-08-03 00:00:00"]
-        name = [None, "自动化"]
-        auditStatus = [None, "AUDIT_ING", "AUDIT_SUCCESS", "AUDIT_FAILURE"]
+    @pytest.mark.parametrize("gmtStartDate",[None, "2020-08-03 00:00:00"])
+    @pytest.mark.parametrize("gmtEndDate",[None, "2021-08-03 00:00:00"])
+    @pytest.mark.parametrize("name",[None, "自动化"])
+    @pytest.mark.parametrize("auditStatus",[None, "AUDIT_ING", "AUDIT_SUCCESS", "AUDIT_FAILURE"])
+    def test_MaterialList(self,getheaders,gmtStartDate,gmtEndDate,name,auditStatus):
         params = {
-            "gmtStartDate": random.choice(gmtStartDate),
-            "gmtEndDate": random.choice(gmtEndDate),
-            "name": random.choice(name),
-            "auditStatus": random.choice(auditStatus),
+            "gmtStartDate": gmtStartDate,
+            "gmtEndDate": gmtEndDate,
+            "name": name,
+            "auditStatus": auditStatus,
             "shopId": 0,
             "pageNum": 1,
             "pageSize": 10
@@ -68,23 +67,26 @@ class TestSms:
         assert res.status_code == 200
         assert res.json()['status'] == 0
 
-    def test_05_PlanList(self, getheaders):
+
+
+    @pytest.mark.parametrize("startDate",[None, "2020-08-03 00:00:00"])
+    @pytest.mark.parametrize("endDate",[None, "2021-08-03 00:00:00"])
+    @pytest.mark.parametrize("name",[None, "自动化"])
+    @pytest.mark.parametrize("putStatus",[None, "NOT_PUT", "RESCINDED", "DONE"])
+    @pytest.mark.parametrize("putScene", ["PULL_NEW", "NEW", "RE_PURCHASE", "PROMOTION", "OTHER"])
+    def test_PlanList(self, getheaders,startDate,endDate,name,putStatus,putScene):
         '''短信--计划列表'''
         url = "http://test.shulanchina.cn" + "/api/market/plan/list"
-        startDate = [None, "2020-08-03 00:00:00"]
-        endDate = [None, "2021-08-03 00:00:00"]
-        name = [None, "自动化"]
-        putStatus = [None, "NOT_PUT", "RESCINDED", "DONE"]
-        putScene = ["PULL_NEW", "NEW", "RE_PURCHASE", "PROMOTION", "OTHER"]
+
 
         params = {
-            "name": random.choice(name),
+            "name": name,
             "auditStatus": None,
-            "startDate": random.choice(startDate),
-            "endDate": random.choice(endDate),
+            "startDate": startDate,
+            "endDate": endDate,
             "type": None,
-            "putStatus": random.choice(putStatus),
-            "putScene": random.choice(putScene),
+            "putStatus": putStatus,
+            "putScene": putScene,
             "shopId": 0,
             "pageNumber": 1,
             "pageSize": 10
@@ -95,7 +97,7 @@ class TestSms:
         assert res.json()['status'] == 0
 
 
-    def test_06_PlanDetail(self,getplanId,getheaders):
+    def test_PlanDetail(self,getplanId,getheaders):
         '''短信--计划详情'''
         url="http://test.shulanchina.cn"+"/api/market/plan/detail"
         params=getplanId
@@ -103,7 +105,7 @@ class TestSms:
         assert res.status_code == 200
         assert res.json()['status'] == 0
 
-    def test_07_SmslogList(self,getheaders):
+    def test_SmslogList(self,getheaders):
         '''短信--运营报告列表'''
         url = "http://test.shulanchina.cn"+"/api/market/smslog/list"
         name = random.choice([None, "自动化"])
