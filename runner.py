5d1b400ca83fff032e03c26566f69df8e7e43ac4
18616753564 <m18616753564@163.com>
2021-03-11 17:56:40 +0800

    1344

--- runner.py
+++ runner.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
     # pytest.main(["--html=report/reportname.html", './test/test_sceneAndGoods.py','-n=1'])
     os.environ['--env'] = 'test'
     # pytest.main(["-s", './test/test_appCircle.py', '-n=1'])
-    pytest.main(
-        [f"--html=report/API_test_report_{datetime.strftime(datetime.now(), '%Y-%m-%d')}.html", '-n=2','-l',
-         './test/'])
+    #pytest.main([f"--html=report/API_test_report_{datetime.strftime(datetime.now(), '%Y-%m-%d')}.html", '-n=2','-l','./test/'])
+
+    pytest.main(["--html=report/reportname.html", './test/test_shoppingMall', '-n=1'])
+
+
     # pytest.main(["-s", './test/test_popPortrait.py'])
